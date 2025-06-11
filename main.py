05f50c1ea6636236fc355ba43e061758eca78145
Juanito <juan.david.cuellar@correounivalle.edu.co>
2025-06-11 17:09:01 -0500

    se quita la consola :D

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# main.py
-
-def menu():
-    print("===== Pokeminmax =====")
-    print("1. Jugar en consola")
-    print("2. Jugar con interfaz gráfica")
-    opcion = input("Elige una opción: ")
-
-    if opcion == "1":
-        from ui.console_ui import combate
-        combate()
-    elif opcion == "2":
-        from ui.graphic_ui import combate_grafico
-        combate_grafico()
-    else:
-        print("Opción no válida.")
-
+from ui.graphic_ui import combate_grafico
 if __name__ == "__main__":
-    menu()
-
+    combate_grafico()
